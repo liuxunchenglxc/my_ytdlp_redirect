363c56bdbc3bce11786b6d7bdf6ffa16345f29a9
刘迅承 <33124039+liuxunchenglxc@users.noreply.github.com>
2026-04-17 00:43:23 +0800

    Update my-script.sh

--- my-script.sh
+++ my-script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-chmod +x yt-dlp_linux
 url='https://www.youtube.com/watch?v=DmFdxaLgD70'
-./yt-dlp_linux -N 4 --extractor-args "generic:impersonate" --embed-thumbnail --embed-metadata --audio-quality 0 $url
+./yt-dlp_linux --cookies cookies.txt -N 4 --extractor-args "generic:impersonate" --embed-thumbnail --embed-metadata --audio-quality 0 $url
 ls .
