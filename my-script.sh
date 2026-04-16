2c12ed0b7df53bc4dec3df272c812926b4ce246a
刘迅承 <33124039+liuxunchenglxc@users.noreply.github.com>
2026-04-17 00:52:00 +0800

    Update my-script.sh

--- my-script.sh
+++ my-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 url='https://www.youtube.com/watch?v=DmFdxaLgD70'
-./yt-dlp_linux --cookies cookies.txt -N 4 --extractor-args "generic:impersonate" --embed-thumbnail --embed-metadata --audio-quality 0 $url
+./yt-dlp_linux --cookies cookies.txt --remote-components ejs:github -N 4 --extractor-args "generic:impersonate" --embed-thumbnail --embed-metadata --audio-quality 0 $url
 rm cookies.txt
 ls .
